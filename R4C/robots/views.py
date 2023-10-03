39bb7fd8c5a00d0758cafa1a143530c9d44a95b8
Vlad <yashenkov.v@yandex.ru>
2023-10-03 11:24:31 +0300

    task 2 done

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+from .services import make_excel_file
+
+
+#Page with link to download excel file
+def excel_link(request):
+    return render(request, 'robots/download_excel.html')
+
+
+def download_excel(_):
+
+    make_excel_file()
+
+    with open('excel_file.xlsx', 'rb') as f:
+        response = HttpResponse(f, content_type='application/force-download')
+        response['Content-Disposition'] = 'attachment; filename="File.xlsx"'
+
+    if os.path.exists('excel_file.xlsx'):
+        os.remove('excel_file.xlsx')
+    
+    return response
